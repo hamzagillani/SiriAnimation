a58b77a0c654655583d9f7d4ba91f2e5b254748c
Hamza Gillani <hamzagillani10@gmail.com>
2019-07-01 22:22:43 -0700

    Initial commit

---
+++ app/src/main/java/com/dogtranslator/MainActivity.java
@@ -0,0 +1,46 @@
+package com.dogtranslator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+
+import com.dnkilic.waveform.WaveView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WaveView mWaveView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mWaveView=findViewById(R.id.vw2);
+
+
+
+    }
+
+
+    public void reset(View v) {
+        mWaveView.stop();
+    }
+
+    public void start(View v) {
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        mWaveView.initialize(dm);
+    }
+
+    public void startSpeech(View v) {
+        mWaveView.speechStarted();
+    }
+
+    public void endSpeech(View v) {
+        mWaveView.speechEnded();
+    }
+
+    public void pauseSpeech(View v) {
+        mWaveView.speechPaused();
+    }
+}
